02b2d3bed9461618f51e584d48defde7ac0c35fc
a11arjunyadav <yadavarjun0312@gmail.com>
2023-09-28 10:58:11 +0530

    ca01

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+console.log("External file");
+
+var aa = 1;
+var d = 1;
+var bb= 1;
---
+++ js/text.js
@@ -0,0 +1,14 @@
+//1. veriable declaration
+var x;
+
+
+//2. veriable initalation
+var y;
+x = 20;
+
+
+//3. veriable declaration and initialization
+var z = 10;
+
+
+console.log(x+z);
